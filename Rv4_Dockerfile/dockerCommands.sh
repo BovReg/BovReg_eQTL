a3746895b98061450ee58c6d85142c1316e5fd19
Praveen-Chitneedi <74717722+Praveen-Chitneedi@users.noreply.github.com>
2023-09-15 12:45:30 +0200

    Update dockerCommands.sh

--- Rv4_Dockerfile/dockerCommands.sh
+++ Rv4_Dockerfile/dockerCommands.sh
@@ -7,12 +7,11 @@ docker build -t praveen/rpackages-eqtl .
 
 # Run docker r studio
 
-docker run -it --rm --user rstudio praveen/rpackages-eqtl R
+#docker run -it --rm --user rstudio praveen/rpackages-eqtl R
 
 # Run bash script
-
-docker run -it --rm --user rstudio praveen/rpackages-eqtl bash
+#docker run -it --rm --user rstudio praveen/rpackages-eqtl bash
 
 # Mount directory and set working directory
-docker run -it --rm -v $(pwd):/disk2/chitneedi/NextFlow/eQTL_Genotype_vcf/liver_Test_Genotypes -w /disk2/chitneedi/NextFlow/eQTL_Genotype_vcf/liver_Test_Genotypes praveen/rpackages-eqtl R
+#docker run -it --rm -v $(pwd):/disk2/chitneedi/NextFlow/eQTL_Genotype_vcf/liver_Test_Genotypes -w /disk2/chitneedi/NextFlow/eQTL_Genotype_vcf/liver_Test_Genotypes praveen/rpackages-eqtl R
 
